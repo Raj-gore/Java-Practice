29de64b7410ab57280cfe71cd41dffd0d5d1d507
Raj Gore <rajgore220@gmail.com>
2026-07-08 16:15:05 +0530

    Add Pattern5.java

---
+++ Pattern-Programs/Alphabet-Patterns/Pattern5.java
@@ -0,0 +1,19 @@
+//E
+//ED
+//EDC
+//EDCB
+//EDCBA
+
+class Pattern5 {
+    public static void main(String args[]){
+        int n = 5;
+        for(int i = n; i >= 1; i--)
+        {
+            for(int j = n;j >= i; j--)
+            {
+                System.out.print((char)(j+64));
+            }     
+            System.out.println();
+        }
+    }   
+}
