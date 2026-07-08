666da28c57469bdc3911b6ca972050658a293aa9
Raj Gore <rajgore220@gmail.com>
2026-07-08 18:07:21 +0530

    Add Pattern8.java

---
+++ Pattern-Programs/Alphabet-Patterns/Pattern7.java
@@ -0,0 +1,19 @@
+//AAAAAA
+//BBBB
+//CCC
+//DD
+//E
+
+class Pattern7 {
+    public static void main(String args[]){
+        int n = 5;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = n;j >= i; j--)
+            {
+                System.out.print((char)(i+64));
+            }    
+            System.out.println();
+        }
+    }   
+}
