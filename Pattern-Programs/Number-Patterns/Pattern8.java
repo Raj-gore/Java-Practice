7f64a315593e84388d8609c350548bd13af96897
Raj-gore <rajgore220@gmail.com>
2026-07-10 17:41:03 +0530

    Update Pattern8.java

--- Pattern-Programs/Number-Patterns/Pattern8.java
+++ Pattern-Programs/Number-Patterns/Pattern8.java
@@ -9,7 +9,7 @@ class Pattern8 {
         int n = 5;
         for(int i = n; i >= 1; i--)
         {
-            for(int j = 1 ; j <= i; j++)
+            for(int j = i ; j >= 1; j--)
             {
                 System.out.print(i);
             } 
