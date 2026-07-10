7040f7c5c5c66d3752415219f2c65d068fc6b0d1
Raj Gore <rajgore220@gmail.com>
2026-07-10 16:12:10 +0530

    Add Pattern11.java

---
+++ Pattern-Programs/Space-Patterns/Alphabet patterns with spaces/Pattern11.java
@@ -0,0 +1,30 @@
+//EDCBA
+// EDCB
+//  EDC
+//   ED 
+//    E   
+     
+class Pattern11
+{
+	public static void main(String args[])
+	{
+		int n =5;
+		for (int i=1; i <= n; i++ )
+		{
+			for (int j=1; j < i ; j++)
+			{
+				System.out.print(" ");							
+			}
+			for(int j=n; j >= i; j--)
+			{	
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+
+
+
+		}
+
+	}
+}
+           
