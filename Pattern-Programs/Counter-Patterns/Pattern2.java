e7a01b0cd65b0531e8266d8446564fa3d608747d
Raj Gore <rajgore220@gmail.com>
2026-07-12 12:07:31 +0530

    Add Pattern2.java

---
+++ Pattern-Programs/Counter-Patterns/Pattern2.java
@@ -0,0 +1,23 @@
+// 15
+// 14 13
+// 12 11 10 
+// 9  8  7  6
+// 5  4  3  2  1
+
+class Pattern2
+{
+    public static void main(String Args[])
+    {
+        int n = 5;
+        int count = n * (n + 1) / 2;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j =i ; j >=1 ; j--)
+            {
+                System.out.print(count + "\t");
+                count--;
+            }   
+        System.out.println( );    
+        }    
+    }
+}
