8107c663950423dc99693346510097a52ab15cc6
Raj Gore <rajgore220@gmail.com>
2026-07-13 16:01:22 +0530

    Add Pattern3.java

---
+++ Pattern-Programs/Counter-Patterns/Pattern3.java
@@ -0,0 +1,25 @@
+// 1
+// 3   2
+// 6   5  4
+// 10  9  8  7  
+// 15 14 13 12 11
+
+class Pattern3
+{
+    public static void main(String Args[])
+    {
+        int n = 5;
+        int count = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            int c = count;
+            for(int j =1 ; j <=i ; j++)
+            {
+                System.out.print(c + "\t");
+                c -- ; 
+            }   
+        System.out.println( ); 
+        count += i + 1;   
+        }    
+    }
+}
