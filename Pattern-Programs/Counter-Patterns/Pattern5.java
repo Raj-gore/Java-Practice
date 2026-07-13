3d8922b544c419e55ba27dfb5a830a6068e36bc0
Raj Gore <rajgore220@gmail.com>
2026-07-13 16:29:50 +0530

    Add Pattern5.java

--- Pattern-Programs/Counter-Patterns/Pattern5.java
+++ Pattern-Programs/Counter-Patterns/Pattern5.java
@@ -1,22 +1,22 @@
-// 1
-// 2  6
-// 3  7  10
-// 4  8  11  13  
-// 5  9  12  14  15
+// 15
+// 13  14
+// 10  11  12
+//  6   7   8  9  
+//  1   2   3  4  5
 
 class Pattern5
 {
-    public static void main(String args[])
+    public static void main(String Args[])
     {
         int n = 5;
-        int count = 1;
+        int count = (n * (n + 1)) / 2;
         for(int i = n; i >= 1; i--)
         {
             int c = count;
             for(int j =n ; j >=i ; j--)
             {
                 System.out.print(c + "\t");
-                c += j - 1; 
+                c + = j - 1; 
             }   
         System.out.println( ); 
         count ++;   
