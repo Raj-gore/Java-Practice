89e4a3f82ba19f14502fc45bdc89640275d9e344
Raj Gore <rajgore220@gmail.com>
2026-07-14 00:06:14 +0530

    Add Pattern13.java

---
+++ Pattern-Programs/Counter-Patterns/Pattern13.java
@@ -0,0 +1,25 @@
+//  1    2   3    4   5
+//  6    7   8    9
+// 10   11  12
+// 13   14    
+// 15       
+
+class Pattern13
+{
+    public static void main(String Args[])
+    {
+        int n = 5;
+        int count = 1;
+        for(int i = n; i >= 1; i--)
+        {
+            int c = count;
+            for(int j = i; j >= 1; j--)
+            {
+                System.out.print(c + "\t");
+                c ++; 
+            }   
+        System.out.println( ); 
+        count += i;   
+        }    
+    }
+}
