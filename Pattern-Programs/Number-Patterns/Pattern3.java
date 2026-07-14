feda07a1a5f45d6d57b24e60b2315687b753c33d
Raj Gore <rajgore220@gmail.com>
2026-07-14 11:47:30 +0530

    Remove .class files and add .gitignore

--- Pattern-Programs/Number-Patterns/Pattern3.java
+++ Pattern-Programs/Number-Patterns/Pattern3.java
@@ -10,8 +10,7 @@ class Pattern3 {
         int n = 5;
         for(int i = 1; i <= n; i++)
         {
-            for(int j = 1;j <= i; j++ )
-            {
+            for(int j = 1; j <= i; j++){
                 System.out.print(j);
             }     
             System.out.println();
