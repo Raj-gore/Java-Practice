d7eba0a5c7b0d66563592c169a1561f395edec88
Raj Gore <rajgore220@gmail.com>
2026-07-14 09:59:17 +0530

    Add Pattern22.java

---
+++ Pattern-Programs/Counter-Patterns/Pattern22.java
@@ -0,0 +1,25 @@
+// 1   2   4   7   11
+// 3   5   8   12  
+// 6   9   13
+// 10  14
+// 15
+
+class Pattern22
+{
+    public static void main(String Args[])
+    {
+        int n = 5;
+        int count = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            int c = count;
+            for(int j = i; j <= n; j++)
+            {
+                System.out.print(c + "\t");
+                c += j; 
+            }   
+        System.out.println( ); 
+        count += i+1;   
+        }    
+    }
+}
