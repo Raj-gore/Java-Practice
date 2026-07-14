dd08d58e22aa250d34d4f4bd6bc50373a7337ce9
Raj Gore <rajgore220@gmail.com>
2026-07-14 15:19:35 +0530

    Add Pattern4

---
+++ Pattern-Programs/Counter-Patterns/alphabet_counter_patterns;/Pattern4.java
@@ -0,0 +1,25 @@
+// O
+// MN
+// JKL
+// FGHI
+// ABCDE
+
+class Pattern4
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        int count = n*(n+1)/2;
+        for(int i = 1; i <= n; i++)
+        {
+            int c = count;
+            for(int j = 1 ; j <= i ; j++)
+            {
+                System.out.print((char)(c +64));
+                c ++; 
+            }   
+        System.out.println( ); 
+        count -= i+1;   
+        }    
+    }
+}
