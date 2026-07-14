46a559a75686b0051c58d59fe53f651ce8cad33f
Raj Gore <rajgore220@gmail.com>
2026-07-14 12:20:11 +0530

    Pattern1.java

---
+++ Pattern-Programs/Counter-Patterns/alphabet_counter_patterns;/Pattern1.java
@@ -0,0 +1,25 @@
+// A
+// BC
+// DEF
+// GHIJ
+// KLMNO
+
+class Pattern1
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        int count = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            int c = count;
+            for(int j = 1 ; j <= i ; j++)
+            {
+                System.out.print((char)(c +64));
+                c ++; 
+            }   
+        System.out.println( ); 
+        count +=i;   
+        }    
+    }
+}
