f3c8070b666aa4c74d9a992ff197c5d19295ca21
Raj Gore <rajgore220@gmail.com>
2026-07-15 16:42:02 +0530

    Add Pattern5

---
+++ Pattern-Programs/Alphabet_space_counter_Patterns/Pattern5.java
@@ -0,0 +1,29 @@
+//     A
+//    BF
+//   CGJ
+//  DHKM
+// EILNO
+
+class Pattern5
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        int count =1;
+        for(int i = n; i >= 1; i--)
+        {
+            int c = count;
+            for(int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = n; j >=i;j--)
+            {
+                System.out.print((char)(c+64));
+                c+=j-1;
+            }     
+        System.out.println( ); 
+        count ++;   
+        }    
+    }
+}
