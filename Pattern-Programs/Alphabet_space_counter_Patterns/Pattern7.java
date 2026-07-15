99fbb02bbf07d1cf050be61540d3ab3e77604626
Raj Gore <rajgore220@gmail.com>
2026-07-15 16:52:07 +0530

    Add Pattern7

---
+++ Pattern-Programs/Alphabet_space_counter_Patterns/Pattern7.java
@@ -0,0 +1,29 @@
+//     O
+//    NJ
+//   MIF
+//  LHEC
+// KGDBA
+
+class Pattern7
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        int count =n*(n+1)/2;
+        for(int i = n; i >= 1; i--)
+        {
+            int c = count;
+            for(int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = n; j >=i;j--)
+            {
+                System.out.print((char)(c+64));
+                c-=j-1;
+            }     
+        System.out.println( ); 
+        count --;   
+        }    
+    }
+}
