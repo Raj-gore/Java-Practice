73d6f7d545c75fa3a1841c8e06871a43039ead0d
Raj Gore <rajgore220@gmail.com>
2026-07-15 07:46:49 +0530

    Add Pattern2

--- Pattern-Programs/Counter-Patterns/space_counter_patterns/Pattern2.java
+++ Pattern-Programs/Counter-Patterns/space_counter_patterns/Pattern2.java
@@ -1,15 +1,15 @@
-//                 1
-//             2   3
-//         4   5   6   
-//     7   8   9   10
-// 11  12  13  14  15
+//                 15
+//             14  13  
+//         12  11  10
+//     9   8   7   6   
+// 5   4   3   2   1   
 
-class Pattern1
+class Pattern2
 {
     public static void main(String args[])
     {
         int n = 5;
-        int count =1;
+        int count =n*(n+1)/2;
         for(int i = 1; i <= n; i++)
         {
             int c = count;
@@ -20,10 +20,10 @@ class Pattern1
             for(int j = i; j >=1;j--)
             {
                 System.out.print(c + "\t");
-                c++;
+                c--;
             }     
         System.out.println( ); 
-        count +=i;   
+        count -=i;   
         }    
     }
 }
