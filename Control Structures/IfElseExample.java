6d741cb676870cd2b754c8bb6c85db6ca84d582a
Raj Gore <rajgore220@gmail.com>
2026-07-16 15:27:17 +0530

    IfElseExample

---
+++ Control Structures/IfElseExample.java
@@ -0,0 +1,14 @@
+public class IfElseExample {
+    public static void main(String args[])
+	{
+	    int n = -10;
+		
+	    if(n > 0)	
+	    {
+	        System.out.println("n is Positive");
+	    }
+        else
+            System.out.println("n is Negative");
+	}
+
+}
