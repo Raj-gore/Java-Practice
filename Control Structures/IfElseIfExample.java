65223f239ba43e68d39307d988b605bd442cfe9f
Raj Gore <rajgore220@gmail.com>
2026-07-16 15:55:19 +0530

    IfElseIfExample

---
+++ Control Structures/IfElseIfExample.java
@@ -0,0 +1,23 @@
+public class IfElseIfExample {
+    public static void main(String args[])
+    {
+        int marks = 81;
+
+        if(marks > 90)
+        {
+            System.out.print("Grade A");
+        } 
+        else if(marks > 70)
+        {
+            System.out.print("Grade B");
+        }
+        else if(marks > 50)
+        {
+            System.out.print("GRade C");
+        }  
+        else
+        {
+            System.out.print("Fail");
+        }                          
+    }
+}
