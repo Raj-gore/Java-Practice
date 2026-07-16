9b740c09fcf17e88202e7417a3c6b823ad4872c3
Raj Gore <rajgore220@gmail.com>
2026-07-16 18:31:01 +0530

    Add Pattern2.java

---
+++ Pattern-Programs/Conditional-Patterns/Rectangle/Pattern2.java
@@ -0,0 +1,26 @@
+// 11111
+// 00000
+// 11111
+// 00000
+// 11111
+
+public class Pattern2
+{
+    public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = 1; i <= n; i++)
+		{
+			for(int j = 1; j <= n; j++)
+			{
+			if(i % 2 != 0)
+				System.out.print(1);
+			else
+				System.out.print(0);
+			}
+        System.out.println( );
+		}
+	}
+
+}
+   
