4bf20072daa5b7275c6a7aae077a6f0e5186a0cd
Raj Gore <rajgore220@gmail.com>
2026-07-17 11:32:06 +0530

    Add Pattern9

---
+++ Pattern-Programs/Rectangle/Pattern9.java
@@ -0,0 +1,25 @@
+//11111
+//22221
+//33321
+//44321
+//54321
+
+class Pattern9
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = 1 ; i <= n; i++)
+		{
+			for(int j = i; j <= n; j++)
+			{
+			System.out.print(i);
+			}
+			for(int j = i-1; j >= 1; j-- )
+			{
+			System.out.print(j);
+			}			
+		System.out.println( );
+		}
+	}
+}
