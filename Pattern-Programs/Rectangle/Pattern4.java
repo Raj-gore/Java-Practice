ce4b56e2bf146cb4463a48b96d75d8f0bcc974ef
Raj Gore <rajgore220@gmail.com>
2026-07-17 09:09:42 +0530

    Add Pattern4.java

---
+++ Pattern-Programs/Rectangle/Pattern4.java
@@ -0,0 +1,21 @@
+//54321
+//54321
+//54321
+//54321
+//54321
+
+class Pattern4
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = n; i >= 1; i--)
+		{
+			for(int j = n; j >= 1; j--)
+			{
+				System.out.print(j);
+			}
+		System.out.println( );
+		}
+	}
+}
