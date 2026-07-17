f86205ba844e93a7b3d7fcabe0355771e26a69eb
Raj Gore <rajgore220@gmail.com>
2026-07-17 16:26:09 +0530

    Add Pattern11

---
+++ Pattern-Programs/Rectangle/Pattern11.java
@@ -0,0 +1,25 @@
+//54321
+//54322
+//54333
+//54444
+//55555
+
+class Pattern11
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = 1 ; i <= n; i++)
+		{
+			for(int j = n; j > i; j--)
+			{
+			System.out.print(j);
+			}
+			for(int j = i; j >= 1; j-- )
+			{
+			System.out.print(i);
+			}			
+		System.out.println( );
+		}
+	}
+}
