6605babaae63340ee76c31c82d67631c6bfdec25
Raj Gore <rajgore220@gmail.com>
2026-07-17 16:56:41 +0530

    Add Pattern14

---
+++ Pattern-Programs/Rectangle/Pattern14.java
@@ -0,0 +1,25 @@
+//12345
+//22345
+//33345
+//44445
+//55555
+
+class Pattern14
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = 1 ; i <= n; i++)
+		{
+			for(int j = i; j > 1; j--)
+			{
+			System.out.print(i);
+			}
+			for(int j = i; j <= n; j++ )
+			{
+			System.out.print(j);
+			}			
+		System.out.println( );
+		}
+	}
+}
