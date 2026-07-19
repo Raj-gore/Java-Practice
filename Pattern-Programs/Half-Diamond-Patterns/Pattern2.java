f2a3d9bdebdcfb5ef6f0e17afe2be40d0decc5c4
Raj Gore <rajgore220@gmail.com>
2026-07-19 17:53:06 +0530

    Add Pattern2

---
+++ Pattern-Programs/Half-Diamond-Patterns/Pattern2.java
@@ -0,0 +1,38 @@
+/*
+
+1
+22
+333
+4444
+55555
+4444
+333
+22
+1
+
+*/
+
+class Pattern2
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i = 1; i < n; i++)
+		{
+			for(int j = i; j >= 1; j--)
+			{
+			System.out.print(i);
+			}
+		System.out.println( );	
+		}
+		for(int i = n; i >= 1; i--)
+		{
+			for(int j = i; j >= 1; j--)
+			{
+			System.out.print(i);
+			}
+		System.out.println( );	
+		}
+
+	}
+} 
