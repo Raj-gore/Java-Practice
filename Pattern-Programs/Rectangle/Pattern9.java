5877814e9ca41c2da0f2b46484d9f134b70f9cc8
Raj Gore <rajgore220@gmail.com>
2026-07-20 15:37:08 +0530

    Update Pattern9

--- Pattern-Programs/Rectangle/Pattern9.java
+++ Pattern-Programs/Rectangle/Pattern9.java
@@ -11,11 +11,11 @@ class Pattern9
 		int n = 5;
 		for(int i = 1 ; i <= n; i++)
 		{
-			for(int j = i; j <= n; j++)
+			for(int j = i; j < n; j++)
 			{
 			System.out.print(i);
 			}
-			for(int j = i-1; j >= 1; j-- )
+			for(int j = i; j >= 1; j-- )
 			{
 			System.out.print(j);
 			}			
