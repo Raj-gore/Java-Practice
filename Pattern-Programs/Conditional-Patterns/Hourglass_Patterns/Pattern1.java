2032cad9b8fd6fb3b4f9a5f118fa0eb08035f9a8
Raj Gore <rajgore220@gmail.com>
2026-07-20 22:49:19 +0530

    Add Hourglass Patterns

---
+++ Pattern-Programs/Conditional-Patterns/Hourglass_Patterns/Pattern1.java
@@ -0,0 +1,31 @@
+/*
+
+*****
+****
+***
+**
+*
+**
+***
+****
+*****
+
+*/
+
+class Pattern1
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        for(int i = 1; i <= (n*2)-1; i++)
+        {
+            if(i <= n)
+                for(int j = i; j <= n; j++)
+                    System.out.print("*");
+            else
+                for(int j = 1; j <= i - n + 1; j++)
+                    System.out.print("*");
+            System.out.println( );
+        }
+    }
+}
