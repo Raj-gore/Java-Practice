5cbd10d90a95449a6ee27b509f20aea8658fcd1c
Raj Gore <rajgore220@gmail.com>
2026-07-20 11:02:27 +0530

    Pattern8

---
+++ Pattern-Programs/Conditional-Patterns/Half-Diamond-Patterns/Pattern8.java
@@ -0,0 +1,32 @@
+/*
+
+A
+bb
+CCC
+dddd
+EEEEE
+dddd
+CCC
+bb
+A
+
+*/
+class Pattern8
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+
+        for(int i = 1; i <= (n * 2) - 1; i++)
+        {
+            if(i <= n)
+                for(int j = i; j >= 1; j--)
+                    System.out.print((char)(i + 64));
+            else
+                for(int j = i; j <=(n*2)-1 ; j++)
+                    System.out.print((char)((n*2-i) + 64));
+
+            System.out.println();
+        }
+    }
+}
