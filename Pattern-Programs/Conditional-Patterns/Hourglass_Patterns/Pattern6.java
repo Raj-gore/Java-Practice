59c2f26f30231dc296f26bf35819f49f1315161f
Raj Gore <rajgore220@gmail.com>
2026-07-21 10:12:51 +0530

    Add Pattern6

---
+++ Pattern-Programs/Conditional-Patterns/Hourglass_Patterns/Pattern6.java
@@ -0,0 +1,38 @@
+/*
+
+EEEEE
+dddd
+CCC
+bb
+A
+bb
+CCC
+dddd
+EEEEE
+
+*/
+
+class Pattern6
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        for(int i = 1; i <= (n*2)-1; i++)
+        {
+            if(i <= n)
+                for(int j = i; j <= n; j++)
+                    if(i % 2 != 0)
+                        System.out.print((char)(n-i+1+64));
+                    else
+                        System.out.print((char)(n-i+1+96));       
+            else
+                for(int j = 1; j <= i-n+1; j++)
+                    if(i % 2 != 0)
+                        System.out.print((char)(i-n+1+64));
+                    else
+                        System.out.print((char)(i-n+1+96));
+                   
+            System.out.println( );
+        }
+    }
+}
