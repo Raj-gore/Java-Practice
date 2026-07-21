78ed211a78b9693f777269166b0a4dd8d8137a95
Raj Gore <rajgore220@gmail.com>
2026-07-21 11:34:19 +0530

    Add Space-Half-Diamond-Patterns Folder

---
+++ Pattern-Programs/Space-Half-Diamond-Patterns/Pattern1.java
@@ -0,0 +1,39 @@
+/* 
+    *
+   **
+  ***
+ ****
+*****
+ ****
+  *** 
+   **
+    *
+
+*/
+class Pattern1
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i = 1; i <= (n*2)-1; i++)
+        {
+            if(i <= n)
+            {
+                for(int j = i; j < n; j++)
+                    System.out.print( );
+                for(int i = i; i >= 1; i--)
+                    System.out.print("*");
+            } 
+            else  
+                 {
+                for(int j = i; j > n; j--)
+                    System.out.print( );
+                for(int j = i; j <= n*2-1; j++)
+                    System.out.print("*");
+            } 
+            System.out.println( );
+           
+            
+        }
+    }
+}
