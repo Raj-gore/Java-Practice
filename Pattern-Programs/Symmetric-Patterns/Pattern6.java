d6022b2fdb29d7548480ace06c956bf74fe35f5b
Raj Gore <rajgore220@gmail.com>
2026-07-24 19:15:21 +0530

    Add Pattern7

---
+++ Pattern-Programs/Symmetric-Patterns/Pattern6.java
@@ -0,0 +1,60 @@
+/*
+
+EEEEEEEEE
+dddd dddd
+CCC   CCC
+bb     bb
+A       A
+bb     bb
+CCC   CCC
+dddd dddd
+EEEEEEEEE
+
+*/
+
+class Pattern6
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i=n ; i>=1 ; i--)
+		{
+			if( i==n )
+			{
+				for(int j=1; j<=n*2-1 ; j++)
+					System.out.print((char)(i + 64));
+			}
+			else
+			{
+				for(int j=i ; j>=1 ; j--)
+				System.out.print((char)(i + ((i % 2 != 0) ? 64 : 96)));
+				for(int j=1 ; j<=((n-i+1)*2)-3 ; j++)
+					System.out.print(" ");
+				for(int j=i ; j>=1 ; j--)
+					System.out.print((char)(i + ((i % 2 != 0) ? 64 : 96)));
+		    }
+        System.out.println();
+        }
+        for(int i=2 ; i<=n ; i++)
+		{
+			if( i==n )
+			{
+				for(int j=1; j<=n*2-1 ; j++)
+					System.out.print((char)(i + 64));
+			}
+			else
+			{
+				for(int j=i ; j>=1 ; j--)
+				System.out.print((char)(i + ((i % 2 != 0) ? 64 : 96)));
+				for(int j=1 ; j<=((n-i+1)*2)-3 ; j++)
+					System.out.print(" ");
+				for(int j=i ; j>=1 ; j--)
+					System.out.print((char)(i + ((i % 2 != 0) ? 64 : 96)));
+		    }
+        System.out.println();
+        }
+        
+	}
+}
+        
+	
