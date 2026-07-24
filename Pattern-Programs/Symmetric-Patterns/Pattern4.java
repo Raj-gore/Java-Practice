69d75f928a9a9cccc95c358d1b2659f24ab78bc8
Raj Gore <rajgore220@gmail.com>
2026-07-24 09:24:35 +0530

    Add Pattern4

---
+++ Pattern-Programs/Symmetric-Patterns/Pattern4.java
@@ -0,0 +1,55 @@
+// EDCBABCDE
+// EDCB BCDE
+// EDC   CDE 
+// ED     DE 
+// E       E
+
+class Pattern4
+{
+	public static void main(String args[])
+	{
+		int n = 5;
+		for(int i=1 ; i<=n ; i++)
+		{
+			if( i==1)
+			{
+				for(int j=n; j>=i ; j--)
+					System.out.print((char)(j+64));
+				for(int j=2 ; j<=n ; j++)
+					System.out.print((char)(j+64));
+			}
+			else
+			{
+				for(int j=n ; j>=i ; j--)
+					System.out.print((char)(j+64));
+				for(int j=1 ; j<=(i*2)-3 ; j++)
+					System.out.print(" ");
+				for(int j=i ; j<=n ; j++)
+					System.out.print((char)(j+64));
+
+			}
+			System.out.println( );
+		}
+        for(int i=n-1 ; i>=1 ; i--)
+		{
+			if( i==1)
+			{
+				for(int j=n; j>=i ; j--)
+					System.out.print((char)(j+64));
+				for(int j=2 ; j<=n ; j++)
+					System.out.print((char)(j+64));
+			}
+			else
+			{
+				for(int j=n ; j>=i ; j--)
+					System.out.print((char)(j+64));
+				for(int j=1 ; j<=(i*2)-3 ; j++)
+					System.out.print(" ");
+				for(int j=i ; j<=n ; j++)
+					System.out.print((char)(j+64));
+
+			}
+			System.out.println( );
+		}
+	}
+}
