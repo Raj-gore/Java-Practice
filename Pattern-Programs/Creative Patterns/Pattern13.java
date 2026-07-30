776df9d98d8c8500b9b9526d6fafe40c8aa678a9
Raj Gore <rajgore220@gmail.com>
2026-07-30 17:36:40 +0530

    Pattern13

---
+++ Pattern-Programs/Creative Patterns/Pattern13.java
@@ -0,0 +1,28 @@
+/* 
+
+*  ****
+*  *
+*  * 
+*******
+   *  *
+   *  *
+****  *
+
+*/
+ 
+class Pattern13
+{
+    public static void main(String args[])
+    {
+        int n = 7;
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=n ; j++)
+                if(j==1 && i<=(n+1)/2 || j==n&&i>=(n+1)/2 || i==1&&j>=(n+1)/2 || i==n&&j<=(n+1)/2 || i==n-3 || j==(n+1)/2 )
+                    System.out.print( "*");
+                else
+                     System.out.print(" " );
+                System.out.println( );
+        }
+    }
+}
