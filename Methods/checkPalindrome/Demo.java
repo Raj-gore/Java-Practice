361dc99722566b3d5d4be347d57bf1ba98201ebf
Raj Gore <rajgore220@gmail.com>
2026-08-09 09:02:46 +0530

    checkPalindrome

---
+++ Methods/checkPalindrome/Demo.java
@@ -0,0 +1,14 @@
+class Demo
+{
+	boolean checkPalindrome (int n)
+	{
+		int sum = 0 , temp =n ;
+		while(temp != 0)
+		{
+			sum *= 10;
+			sum += temp % 10;
+			temp /= 10;
+		}
+		return sum == n;
+	}
+}
