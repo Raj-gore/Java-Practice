1bf438d68afd4c2ba530e139b1202a8bdc2325f6
Raj Gore <rajgore220@gmail.com>
2026-08-09 09:35:26 +0530

    checkPrime

---
+++ Methods/checkPrime/Demo.java
@@ -0,0 +1,17 @@
+class Demo
+{
+	boolean checkPrime(int n)
+	{
+		boolean b = false;
+		if( n == 0|| n==1)
+			b = true;
+		else
+			for(int i = 2; i <= n/2; i++)
+				if(n % i == 0)
+				{
+					b = true;
+					break;
+				}
+		return ! b;
+	}
+}
