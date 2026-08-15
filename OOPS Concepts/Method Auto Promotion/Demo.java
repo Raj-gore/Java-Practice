bbf219b0ec070463342c2b2f70743ee020c16793
Raj Gore <rajgore220@gmail.com>
2026-08-15 21:50:30 +0530

    Method Auto Promotion

---
+++ OOPS Concepts/Method Auto Promotion/Demo.java
@@ -0,0 +1,7 @@
+class Demo 
+{
+	void add(Object a, Object b)
+	{
+		System.out.println("Object Object add Method called ");
+	}
+}
---
+++ OOPS Concepts/Method Auto Promotion/MainClass.java
@@ -0,0 +1,10 @@
+class MainClass
+{
+	public static void main(String args[])
+	{
+		Demo d1 = new Demo();
+		d1.add(10,"ABC");
+		d1.add(true,"ABC");
+		d1.add(10.5,5.3);
+	}
+}
