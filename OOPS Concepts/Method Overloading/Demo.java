6631dd7053bc001beeb0334bc350dac48bc810a5
Raj Gore <rajgore220@gmail.com>
2026-08-15 21:26:14 +0530

    Method Overloading

---
+++ OOPS Concepts/Method Overloading/Demo.java
@@ -0,0 +1,20 @@
+
+// Method Overloading-
+// in java method overloading is a concept where , one class contain 2 or more method comtain with same name but differnt parameter
+
+
+class Demo
+{
+	void m1()
+	{
+		System.out.println("No arg m1 methos called");
+	}
+	void m1(int a)
+	{
+		System.out.println("int arg m1 methos called");
+	}
+	void m1(int a , int b)
+	{
+		System.out.println("int int arg m1 methos called");
+	}
+}
---
+++ OOPS Concepts/Method Overloading/MainClass.java
@@ -0,0 +1,10 @@
+class MainClass
+{
+	public static void main(String args[])
+	{
+		Demo d1 = new Demo();
+		d1.m1();
+		d1.m1(10);
+		d1.m1(10,20);
+	}
+}
