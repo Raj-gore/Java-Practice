8a26007a5917681bd85767c9c555c234dae0c462
Raj Gore <rajgore220@gmail.com>
2026-08-16 16:54:07 +0530

    instance block

---
+++ OOPS Concepts/Initializers/Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+	{
+        	System.out.println("Instance Block called");
+    	}
+	Demo()
+	{
+		System.out.println("Constructor called");
+	}
+}
---
+++ OOPS Concepts/Initializers/MainClass.java
@@ -0,0 +1,8 @@
+class MainClass 
+{
+	public static void main(String args[])
+	{
+		Demo d1 = new Demo();
+		Demo d2 = new Demo();
+	}
+}
