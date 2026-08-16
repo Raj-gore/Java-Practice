cb3aa05ae2f12582f7c073ae91dabfb618ca1c45
Raj Gore <rajgore220@gmail.com>
2026-08-16 07:40:01 +0530

    default constructor

---
+++ OOPS Concepts/Constructor/Default Constructor/Demo.java
@@ -0,0 +1,14 @@
+class Demo
+{
+	int a ,b;
+	Demo()
+	{
+		a = 10;
+		b = 20;
+		System.out.println("default constructor called");
+	}
+	void display()
+	{
+		System.out.println("a = " +a+ "b = "+b);
+	}
+}
---
+++ OOPS Concepts/Constructor/Default Constructor/MainClass.java
@@ -0,0 +1,12 @@
+class MainClass 
+{
+	public static void main(String args[])
+	{
+		Demo d1 =new Demo();
+		Demo d2 =new Demo();
+		Demo d3 =new Demo();
+		d1.display();
+		d2.display();
+		d3.display();
+	}
+}
