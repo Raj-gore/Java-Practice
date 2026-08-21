3f8cd754be611e1f07c12a8e6a8882ef213f1596
Raj Gore <rajgore220@gmail.com>
2026-08-21 23:43:47 +0530

    By  Using 2D Array

---
+++ Array/HowToCreateArray/Using2DArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class  Using2DArray
+{
+	public static void main (String args[])
+	{
+		int a[][] = new int [5][5];
+		for(int i = 0 ; i <a.length ; i++)
+		{
+			System.out.println(Arrays.toString(a[i]));	
+		}
+	}
+}
