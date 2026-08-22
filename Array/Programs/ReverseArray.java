0fcfcc25f869f0b5311a23da0b8b1e389f25128a
Raj Gore <rajgore220@gmail.com>
2026-08-22 22:57:35 +0530

    Reverse Array

---
+++ Array/Programs/ReverseArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class ReverseArray
+{
+	public static void main(String args[])
+	{
+		int a[] = {10,20,30,40,50};
+		int i = 0 , j = a.length-1 ;
+		while(i<j)
+		{
+			int temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i++;
+			j--;	
+		}	
+		System.out.println(Arrays.toString(a));
+	}
+}
