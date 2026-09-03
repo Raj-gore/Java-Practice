b2eda762730f1e4009d8e7645c08a932525b8fff
Raj Gore <rajgore220@gmail.com>
2026-09-03 22:13:31 +0530

    By using command line argument

---
+++ Input/HowToTakeInput/InputUsingCLA.java
@@ -0,0 +1,11 @@
+// By using command line argument
+
+class InputUsingCLA
+{
+	public static void main(String args[])
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println(a + b);
+	}
+}
