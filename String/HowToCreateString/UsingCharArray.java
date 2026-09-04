0773ad1496d0a08d76da43ebbe7f5a26f88360bc
Raj Gore <rajgore220@gmail.com>
2026-09-04 23:10:32 +0530

    by Using Char[] Array

---
+++ String/HowToCreateString/UsingCharArray.java
@@ -0,0 +1,10 @@
+class UsingCharArray
+{
+	public static void main(String args[])
+	{
+		char c[] = {'I','N','D'};
+		String s1 = new String(c);
+		System.out.println(s1);
+		System.out.println(c);
+	}
+}
