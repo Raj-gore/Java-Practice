491b29892df50d4012137ad7daaa12c2b713bdbe
Raj Gore <rajgore220@gmail.com>
2026-09-08 22:37:34 +0530

    Remove duplicate string programs

--- Input/HowToTakeInput/InputUsingScanner.java
+++ Input/HowToTakeInput/InputUsingScanner.java
@@ -16,3 +16,28 @@ class InputUsingScanner
 		System.out.println(a + b);
 	}
 }
+
+//By using scanner
+
+import java.util.Scanner;
+import java.util.Arrays;
+class InputUsingScanner
+{
+	public static void main(String args[])
+	{
+		Scanner src = new Scanner(System.in);
+
+		System.out.print("Enter size of an array ; ");
+		int a[] = new int[src.nextInt()];
+		for(int i = 0 ; i < a.length ; i++)
+		{
+			System.out.print("Enter element of for a["+i+ "] ");
+			int value = sc.nextInt();
+			if(value == 0)
+				break;
+			a[i] = value;
+		System.out.println(Arrays.toString(a));
+
+		}
+	}
+}
